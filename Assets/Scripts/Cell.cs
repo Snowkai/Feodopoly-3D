a6d0a3727681035fd39d70022452b479977cdaf3
Snowkai <34677392+Snowkai@users.noreply.github.com>
2023-05-17 17:22:22 +0300

    cell modified
    Add empty objects for position fishka

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -10,10 +10,10 @@ public class Cell : MonoBehaviour
     public int cell_price;
     [SerializeField] GameObject Name;
     [SerializeField] GameObject Price;
-    public Vector3 position1;
-    public Vector3 position2;
-    public Vector3 position3;
-    public Vector3 position4;
+    [SerializeField] GameObject Position1;
+    [SerializeField] GameObject Position2;
+    [SerializeField] GameObject Position3;
+    [SerializeField] GameObject Position4;
     // Start is called before the first frame update
     void Start()
     {
