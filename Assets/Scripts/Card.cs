dd7a635ea6e74b7fa63d943fe49960013dc420e0
Snowkai <34677392+Snowkai@users.noreply.github.com>
2023-05-23 18:40:57 +0300

    Add card prefab

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public int card_id;
+    public string card_name;
+    public string card_description;
+    public GameObject Name;
+    public GameObject Description;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Name.GetComponent<TextMeshPro>().text = card_name;
+        Description.GetComponent<TextMeshPro>().text = card_description;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
