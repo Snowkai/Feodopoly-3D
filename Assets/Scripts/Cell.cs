b063ec3ac99a1e512398566a56c2a725eb595de0
Snowkai <34677392+Snowkai@users.noreply.github.com>
2023-06-02 18:25:13 +0300

    Migrate on new device
    Solved all problems with strange behavior editor.

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -9,14 +9,23 @@ public class Cell : MonoBehaviour
     public int cell_id;
     public string cell_name;
     public int cell_price;
+    public GameObject Name_c;
+    public GameObject Price_c;
     public GameObject Position1;
     public GameObject Position2;
     public GameObject Position3;
     public GameObject Position4;
     // Start is called before the first frame update
+
+    private void OnValidate()
+    {
+        Name_c.GetComponent<TextMeshPro>().text = cell_name;
+        Price_c.GetComponent<TextMeshPro>().text = cell_price.ToString();
+    }
     void Start()
     {
-
+        //Name_c.GetComponent<TextMeshPro>().text = cell_name;
+        //Price_c.GetComponent<TextMeshPro>().text = cell_price.ToString();
     }
 
     // Update is called once per frame
